40b522abbc72bd16f1717439168513a9c8e58aa2
GiantTree <webmaster@gianttree.de>
2017-02-12 01:34:31 +0100

    Add README.java to generate a javadoc.

---
+++ src/main/java/org/gtlp/ui/README.java
@@ -0,0 +1,9 @@
+package org.gtlp.ui;
+
+/**
+ * JavaDoc is unavailable due to the fact that this project is written in Kotlin.
+ * <p>
+ * For documentation, please refer to the sources.
+ */
+public class README {
+}
