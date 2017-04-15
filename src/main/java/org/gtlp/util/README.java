a8042325b7c06e7b32ceea657dcd6a92588e421e
GiantTree <webmaster@gianttree.de>
2017-04-15 21:27:01 +0200

    Use Dokka and update library versions. Remove unnecessary files, as well.

--- src/main/java/org/gtlp/util/README.java
+++
@@ -1,9 +0,0 @@
-package org.gtlp.util;
-
-/**
- * JavaDoc is unavailable due to the fact that this project is written in Kotlin.
- * <p>
- * For documentation, please refer to the sources.
- */
-public class README {
-}
